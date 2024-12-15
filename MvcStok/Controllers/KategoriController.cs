a28d0b9395e7ba4d0cd90796636b94668a449ded
nsalkarka <_karka67@windowslive.com>
2024-12-15 04:06:14 +0300

    update

--- MvcStok/Controllers/KategoriController.cs
+++ MvcStok/Controllers/KategoriController.cs
@@ -26,7 +26,33 @@ namespace MvcStok.Controllers
         [HttpPost]
         public ActionResult YeniKategori(TBLKATEGORILER p1)
         {
+            if (!ModelState.IsValid) 
+            {
+                return View("YeniKategori");
+            }
             db.TBLKATEGORILER.Add(p1);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Sil(int id) 
+        { 
+            var kategori=db.TBLKATEGORILER.Find(id);
+            db.TBLKATEGORILER.Remove(kategori);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult KategoriGetir (int id)
+        {
+            var ktgr = db.TBLKATEGORILER.Find(id);
+            return View("KategoriGetir", ktgr);
+        }
+        public ActionResult Guncelle (TBLKATEGORILER p1)
+        {
+            var ktg = db.TBLKATEGORILER.Find(p1.KATEGORIID);
+            ktg.KATEGORIAD=p1.KATEGORIAD;
+
             db.SaveChanges();
             return RedirectToAction("Index");
         }
--- MvcStok/Controllers/MusteriController.cs
+++ MvcStok/Controllers/MusteriController.cs
@@ -16,5 +16,47 @@ namespace MvcStok.Controllers
             var degerler=db.TBLMUSTERILER.ToList();
             return View(degerler);
         }
+
+        [HttpGet]
+        public ActionResult MusteriEkle()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult MusteriEkle(TBLMUSTERILER degerler)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("MusteriEkle");
+            }
+            db.TBLMUSTERILER.Add(degerler);
+            db.SaveChanges();   
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult Sil (int id)
+        {
+            var musteri=db.TBLMUSTERILER.Find(id);
+            db.TBLMUSTERILER.Remove(musteri);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        public ActionResult MusteriGetir(int id)
+        {
+            var mus=db.TBLMUSTERILER.Find(id);
+            return View("MusteriGetir", mus);
+        }
+
+        public ActionResult Guncelle(TBLMUSTERILER p1)
+        {
+            var musteri = db.TBLMUSTERILER.Find(p1.MUSTERIID);
+            musteri.MUSTERIAD=p1.MUSTERIAD;
+            musteri.MUSTERISOYAD=p1.MUSTERISOYAD;
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
     }
 }
